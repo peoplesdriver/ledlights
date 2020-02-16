f3d27f5a597c91f0139c7b85ae3517060b6e38f1
admin <admin@laurentiu.me>
2020-02-16 17:00:04 +0000

    second test

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-import RazLedRoom
+from ota_updater import OTAUpdater
+
+
+def download_and_install_update_if_available():
+    ota_updater = OTAUpdater('https://github.com/peoplesdriver/ledlights.git')
+    ota_updater.download_and_install_update_if_available(ssid, password)
+
+def start():
+    # code goes here ...
+    import wifimgr
+
+    wlan = wifimgr.get_connection()
+
+    if wlan is None:
+        while True:
+            pass  # you shall not pass :D
+
+    import RazLedRoom
+
+
+
+def boot():
+    download_and_install_update_if_available()
+    start()
+
+
+boot()
